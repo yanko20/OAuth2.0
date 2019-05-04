e27170e50e87496bbc190e896793af78bfa71e0e
Yan Komizorov <yanko20@gmail.com>
2019-05-04 16:14:59 -0400

    add import

--- project.py
+++ project.py
@@ -10,6 +10,7 @@ import httplib2
 import json
 from flask import make_response
 import requests
+import string
 
 app = Flask(__name__)
 
@@ -80,8 +81,7 @@ def gconnect():
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
             json.dumps("Token's client ID does not match app's."), 401)
-        print
-        "Token's client ID does not match app's."
+        print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -116,8 +116,7 @@ def gconnect():
     output += login_session['picture']
     output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
-    print
-    "done!"
+    print "done!"
     return output
 
     # DISCONNECT - Revoke a current user's token and reset their login_session
@@ -126,25 +125,19 @@ def gconnect():
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session['access_token']
-    print
-    'In gdisconnect access token is %s', access_token
-    print
-    'User name is: '
-    print
-    login_session['username']
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
     if access_token is None:
-        print
-        'Access Token is None'
+        print 'Access Token is None'
         response = make_response(json.dumps('Current user not connected.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print
-    'result is '
-    print
-    result
+    print 'result is '
+    print result
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
